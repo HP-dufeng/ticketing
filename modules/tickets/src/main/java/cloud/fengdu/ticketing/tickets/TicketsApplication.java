f81078ca44c440886998072a3fd7a9d0e72140fd
fengdu <fengdu_xiangyang@163.com>
2021-10-27 09:41:12 +0800

    add intergation test for create ticket.

---
+++ modules/tickets/src/main/java/cloud/fengdu/ticketing/tickets/TicketsApplication.java
@@ -0,0 +1,14 @@
+package cloud.fengdu.ticketing.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"cloud.fengdu.ticketing" })
+public class TicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsApplication.class, args);
+	}
+}
---
+++ modules/tickets/src/test/java/cloud/fengdu/ticketing/tickets/CreateTicketTest.java
@@ -0,0 +1,27 @@
+package cloud.fengdu.ticketing.tickets;
+
+import org.junit.jupiter.api.Test;
+
+public class CreateTicketTest {
+    
+    @Test
+    public void has_a_resource_api_listening_for_create_ticket() {
+
+    }
+    @Test
+    public void can_only_by_accessed_if_the_user_is_signed_in() {
+
+    }
+    @Test
+    public void returns_an_error_if_an_invalid_title_is_provided() {
+
+    }
+    @Test
+    public void returns_an_error_if_an_invalid_price_is_provided() {
+
+    }
+    @Test
+    public void creates_ticket_with_valid_inputs() {
+
+    }
+}
